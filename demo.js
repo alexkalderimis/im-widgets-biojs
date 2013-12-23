7a10e13b9ef015122fc38f49b4a4780b55a8e9f1
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-12-23 13:55:11 +0000

    More docs, which biojs dislike

--- demo.js
+++ demo.js
@@ -7,6 +7,10 @@ var widget = new Biojs.InterMine.ListAnalysis({
 
 widget.onAllEvents(console.log.bind(console, 'EVENT'));
 
+widget.onClickMatch(function (ident, type) {
+  widget.widgets.imjs.find(type, ident).then(console.log.bind(console, 'FOUND'));
+});
+
 var allWidgets = new Biojs.InterMine.ListAnalysis({
   target: 'all-ws',
   tool: 'all:Gene',
